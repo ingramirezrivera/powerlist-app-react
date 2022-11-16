8b23d728c32a5849bee1937543dab3f79db7f873
ingramirezrivera <ing.ramirezrivera@gmail.com>
2022-11-16 11:51:56 -0500

    Working on add Tasks and delete Task

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
 import React, { useState } from "react";
 import { GiHornedHelm } from "react-icons/gi";
 import { AiOutlinePlus } from "react-icons/ai";
+import { CLOSING } from "ws";
 
 function App() {
-  const [tasks, setTasks] = useState(["Mow the lawn", "Clean the house", "Wash the car"]);
-  const [input, setInput ] = useState("");
+  const [tasks, setTasks] = useState([]);
+  const [input, setInput] = useState("");
+
+  // Add task
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const addTask = {
+      id: Math.floor(Math.random() * 1000),
+      text: input,
+      completed: false,
+    };
+    setTasks([...tasks, addTask]);
+    setInput("");
+  };
+
+  // Delete task
+  const deleteTask = (id) => {
+    console.log(id);
+    console.log(tasks);
+    let filteredTasks = [...tasks].filter((tasks) => tasks.id !== id);
+    setTasks(filteredTasks);
+  };
+
+  // toggle completed Task
+  const toggleComplete = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, completed: !task.completed } : task
+      )
+    );
+    console.log(id);
+    console.log(tasks);
+  };
+
+  const date = new Date()
+  console.log(date)
 
   return (
     <div className="app">
@@ -18,14 +53,29 @@ function App() {
           <AiOutlinePlus />
           <input
             value={input}
-            onChange={e => setInput(e.target.value)}
-            placeholder="Enter Task" type="text" />
+            onChange={(e) => setInput(e.target.value)}
+            placeholder="Enter Task"
+            type="text"
+          />
         </form>
 
         {tasks.map((task) => {
           return (
-            <div>
-              <p>{task}</p>
+            <div key={task.id}>
+              <p
+                onClick={() => {
+                  toggleComplete(task.id);
+                }}
+              >
+                {task.text}{" "}
+              </p>
+              <button
+                onClick={() => {
+                  deleteTask(task.id);
+                }}
+              >
+                Delete
+              </button>
             </div>
           );
         })}
