b136f9c7a9f19411d4928e0e553bc7f4b70e78c8
ingramirezrivera <ing.ramirezrivera@gmail.com>
2022-11-16 07:48:09 -0500

    Continue...

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { GiHornedHelm } from "react-icons/gi";
+import { AiOutlinePlus } from "react-icons/ai";
 
 function App() {
+  const [tasks, setTasks] = useState(["Mow the lawn", "Clean the house", "Wash the car"]);
+  const [input, setInput ] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+        <h1>
+          <GiHornedHelm />
+          Powerlist
+        </h1>
+
+        <form onSubmit={handleSubmit}>
+          <AiOutlinePlus />
+          <input
+            value={input}
+            onChange={e => setInput(e.target.value)}
+            placeholder="Enter Task" type="text" />
+        </form>
+
+        {tasks.map((task) => {
+          return (
+            <div>
+              <p>{task}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
